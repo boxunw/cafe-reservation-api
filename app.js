6f9a5d954b485eb26691fed7c47a1e49624ea6fa
BoxunW <tony1572002@gmail.com>
2023-10-18 02:47:55 +0800

    feat: add routes folder

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const methodOverride = require('method-override')
+const { apis } = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use('/api', apis)
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
