ab4e71d780ec4477c7360459918eea9c16173c64
BoxunW <tony1572002@gmail.com>
2023-10-23 00:39:25 +0800

    feat: add CORS permission

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const { apis } = require('./routes')
+const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// set CORS
+const corsOptions = {
+  origin: [
+    'http://localhost:3000'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(passport.initialize())
 app.use(methodOverride('_method'))
 
 app.use('/api', apis)
