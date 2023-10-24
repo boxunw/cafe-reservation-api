00b11b992e7d3ebb3eff97f61072178a2203afdb
BoxunW <tony1572002@gmail.com>
2023-10-24 18:03:38 +0800

    feat: add times seed file and router modules

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
+const admin = require('./modules/admin')
+const cafes = require('./modules/cafes')
+const favorites = require('./modules/favorites')
+const tables = require('./modules/tables')
+const reservations = require('.modules/reservations')
 const userController = require('../../controllers/user-controller')
 const apiErrorHandler = require('../../middleware/error-handler')
-const { authenticated, authenticatedUser } = require('../../middleware/auth')
+const { authenticated, authenticatedUser, authenticatedAdmin } = require('../../middleware/auth')
 
 router.put('/users/:id/account', authenticated, authenticatedUser, userController.putAccount)
 router.get('/users/:id', authenticated, authenticatedUser, userController.getUser)
 router.post('/signup', userController.signUp)
 router.post('/login', passport.authenticate('local', { session: false }), authenticatedUser, userController.login)
+
+router.user('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/cafes', authenticated, authenticatedUser, cafes)
+router.use('/favorites', authenticated, authenticatedUser, favorites)
+router.use('/tables', authenticated, authenticatedUser, tables)
+router.use('/reservations', authenticated, authenticatedUser, reservations)
+
 router.use('/', apiErrorHandler)
 
 module.exports = router
